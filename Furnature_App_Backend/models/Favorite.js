b33c9fb8dc7e692e51aefde8255c358a07adaaa9
Nardy Michelle <102429234+Nardy11@users.noreply.github.com>
2023-12-01 00:22:33 +0200

    Update Favorite.js

--- Furnature_App_Backend/models/Favorite.js
+++ Furnature_App_Backend/models/Favorite.js
@@ -1,7 +1,7 @@
 const mongoose  = require('mongoose');
 const FavoriteSchema =new mongoose.Schema({
     userId:{type : 'string',required: true},
-    productId:{type : mongoose.Schema.Types.ObjectId,ref: "Product"},
+    productId:{type : mongoose.Schema.Types.ObjectId,ref: "Products"},
 },{timestamps:true});
 
 module.exports =mongoose.model("Favorite",FavoriteSchema)
